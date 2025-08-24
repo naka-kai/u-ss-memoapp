5ab6c9f7ace50d77db65384837c43a5d3f9cb75b
kai <sea.k.naka@gmail.com>
2025-08-24 18:55:25 +0900

    UIをコンポーネントに切り出す

---
+++ src/components/MemoListItem.tsx
@@ -0,0 +1,42 @@
+import { JSX } from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const MemoListItem = (): JSX.Element => {
+  return (
+    <View style={styles.memoListItem}>
+      {/* テキスト部分（左） */}
+      <View>
+        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+        <Text style={styles.memoListItemDate}>2025年10月1日 10:00</Text>
+      </View>
+      {/* バツボタン（右） */}
+      <View>
+        <Text>X</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)'
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
+  }
+})
+
+export default MemoListItem
