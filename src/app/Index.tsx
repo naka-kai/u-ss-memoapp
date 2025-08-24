abae92d4bff72ac032e581ec8a0252d99883ebc1
kai <sea.k.naka@gmail.com>
2025-08-24 16:24:51 +0900

    メモの一覧画面をマークアップ

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -1,12 +1,78 @@
 import { JSX } from "react"
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    // 画面全体
+    <View style={styles.container}>
+
+      {/* ヘッダー */}
+      <View>
+        {/* テキストが入るヘッダー部分 */}
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* メモリスト全体 */}
+      <View>
+
+        {/* メモアイテム */}
+        <View>
+          {/* テキスト部分（左） */}
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年10月1日 10:00</Text>
+          </View>
+          {/* バツボタン（右） */}
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        {/* メモアイテム */}
+        <View>
+          {/* テキスト部分（左） */}
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年10月1日 10:00</Text>
+          </View>
+          {/* バツボタン（右） */}
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        {/* メモアイテム */}
+        <View>
+          {/* テキスト部分（左） */}
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年10月1日 10:00</Text>
+          </View>
+          {/* バツボタン（右） */}
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      {/* プラスボタン */}
+      <View>
+        <Text>＋</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
