aec0fcb46411899e7e640b4e5bcaecb71a294b5d
kai <sea.k.naka@gmail.com>
2025-08-24 16:37:23 +0900

    ヘッダーのスタイリング

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -7,11 +7,11 @@ const Index = (): JSX.Element => {
     <View style={styles.container}>
 
       {/* ヘッダー */}
-      <View>
+      <View style={styles.header}>
         {/* テキストが入るヘッダー部分 */}
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
@@ -70,8 +70,28 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    backgroundColor: '#285DCF',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
   }
 })
 
