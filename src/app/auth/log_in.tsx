1624ee0a34845b96098be90f2879a8d55c39892e
kai <sea.k.naka@gmail.com>
2025-09-06 00:58:15 +0900

    ログイン画面のマークアップ

---
+++ src/app/auth/log_in.tsx
@@ -0,0 +1,31 @@
+import { JSX } from "react"
+import { StyleSheet, Text, TextInput, View } from "react-native"
+import Header from "../../components/Header"
+
+const LogIn = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View>
+        <Text>Log In</Text>
+        <TextInput value="Email Address" />
+        <TextInput value="Password" />
+        <View>
+          <Text>Submit</Text>
+        </View>
+        <View>
+          <Text>Not registered?</Text>
+          <Text>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+export default LogIn
